734e50562b047bd30cf5150e26f07c2e9b07234a
drugovdv <drugovdv76@gmail.com>
2023-03-04 17:21:11 +0300

    nginx

--- mysite/mysite.sh
+++ mysite/mysite.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt install -y nginx
 sudo systemctl enable nginx
-sudo cp -R ~/netology_diplom-main/mysite/My_site /var/www/
+sudo cp -R /home/admin/netology_diplom-main/mysite/My_site /var/www/
 sudo rm -rf /etc/nginx/sites-available/default
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo cp ~/netology_diplom-main/mysite/my_nginx.conf /etc/nginx/sites-available/
+sudo cp /home/admin/netology_diplom-main/mysite/my_nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/my_nginx.conf /etc/nginx/sites-enabled/my_nginx.conf
 sudo mkdir /var/log/nginx/mysite/
 sudo rm -rf /etc/nginx/nginx.conf
-sudo cp ~/netology_diplom-main/mysite/nginx.conf /etc/nginx/
+sudo cp /home/admin/netology_diplom-main/mysite/nginx.conf /etc/nginx/
 sudo systemctl restart nginx
